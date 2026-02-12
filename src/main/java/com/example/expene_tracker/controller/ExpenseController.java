660c23bb4f0abbae9ceebaa43483ec068b2372f2
Mohd Aasif <aasifmohd1385@gmail.com>
2026-02-12 12:10:20 +0530

    add feature getAll and get single expense

--- src/main/java/com/example/expene_tracker/controller/ExpenseController.java
+++ src/main/java/com/example/expene_tracker/controller/ExpenseController.java
@@ -5,6 +5,9 @@ import org.springframework.web.bind.annotation.*;
 import com.example.expene_tracker.entity.Expense;
 import com.example.expene_tracker.service.ExpenseService;
 
+import java.util.List;
+import java.util.Optional;
+
 @RestController
 @RequestMapping("/expenses")
 public class ExpenseController {
@@ -12,12 +15,25 @@ public class ExpenseController {
 
     private final ExpenseService expenseService;
     public ExpenseController(ExpenseService expenseService) {
+
         this.expenseService = expenseService;
     }
 
     @PostMapping
     public Expense addExpense(@RequestBody Expense expense){
+
         return expenseService.addExpense(expense);
     }
+
+    @GetMapping
+    public List<Expense> getAllExpenses(){
+        return expenseService.getExpenses();
+    }
+    @GetMapping("/{id}")
+    public Optional<Expense> getExpenseById(@PathVariable Long id){
+       return expenseService.getExpenseById(id);
+    }
+
+
 
 }
