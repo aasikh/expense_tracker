97906568c3be12824f536863e01ee57737ff610c
Mohd Aasif <aasifmohd1385@gmail.com>
2026-02-19 15:59:21 +0530

    add jwt token for authentication

---
+++ src/main/java/com/example/expene_tracker/util/JwtUtil.java
@@ -0,0 +1,24 @@
+package com.example.expene_tracker.util;
+import com.example.expene_tracker.service.UserService;
+import com.example.expene_tracker.entity.User;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.sql.Timestamp;
+import java.util.Date;
+@Component
+public class JwtUtil {
+    private String secret = "mysecretkeymysecretkeymysecretkeymysecretkeymysecretkey";
+    private long expiration  = 1000 * 60 * 60;
+
+public String genrateToken(User user){
+    return Jwts.builder()
+            .setSubject(String.valueOf(user.getId()))
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + expiration))
+            .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)
+            .compact();
+     }
+}
