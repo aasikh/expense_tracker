7b84bd5473a90d5dadd0fad998b3927a5f766776
Mohd Aasif <aasifmohd1385@gmail.com>
2026-02-23 12:01:19 +0530

    add some method for jwt filter request

--- src/main/java/com/example/expene_tracker/config/SecurityConfig.java
+++ src/main/java/com/example/expene_tracker/config/SecurityConfig.java
@@ -1,13 +1,21 @@
 package com.example.expene_tracker.config;
-
+import com.example.expene_tracker.security.jwtAuthFilter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
+
 @Configuration
 public class SecurityConfig {
+    private final jwtAuthFilter jwtAuthFilter;   // ADD THIS
+
+    public SecurityConfig(jwtAuthFilter jwtAuthFilter) {   // ADD THIS
+        this.jwtAuthFilter = jwtAuthFilter;
+    }
 
     // Your existing password encoder bean
     @Bean
@@ -16,17 +24,17 @@ public class SecurityConfig {
     }
 
     @Bean
-public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
     http
             .csrf(csrf -> csrf.disable())
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
+                    .requestMatchers("/expenses/**").authenticated()
                     .anyRequest().authenticated()
             );
-
+        http.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
     return http.build();
-}
+   }
 
 }
 
-// i just check conflict file code
--- src/main/java/com/example/expene_tracker/util/JwtUtil.java
+++ src/main/java/com/example/expene_tracker/util/JwtUtil.java
@@ -1,6 +1,7 @@
 package com.example.expene_tracker.util;
 import com.example.expene_tracker.service.UserService;
 import com.example.expene_tracker.entity.User;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -21,4 +22,11 @@ public String genrateToken(User user){
             .signWith(Keys.hmacShaKeyFor(secret.getBytes()), SignatureAlgorithm.HS256)
             .compact();
      }
+    public Claims extractClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(secret.getBytes())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
 }
